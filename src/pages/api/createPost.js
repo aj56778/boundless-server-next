6c9f2b0dca829d1514f05fa1501f0e6dc4520710
Arjun Gupta <arjungupta.sljh@gmail.com>
2025-08-03 11:38:43 -0400

    initial push

---
+++ src/pages/api/createPost.js
@@ -0,0 +1,44 @@
+export const runtime = "edge";
+
+export default async function createPost(request) {
+  try {
+    const {
+      userId,
+      images,
+      title,
+      description,
+      rating,
+      location,
+      time,
+      isPublic,
+    } = await request.json();
+
+    console.log({ userId });
+    const db = globalThis.DB; // 'DB' matches your binding name
+
+    const sql = `
+    INSERT INTO reviews (userId, images, title, description, rating, location, time, public)
+    VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+  `;
+
+    const { results } = await db
+      .prepare(sql)
+      .bind(
+        userId,
+        images,
+        title,
+        description,
+        rating,
+        location,
+        time,
+        isPublic ? 1 : 0
+      )
+      .run();
+
+    return new Response(JSON.stringify(results), {
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
